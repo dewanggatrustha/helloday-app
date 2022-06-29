69f03e306ffb43d93f760528a547040b28ebd47e
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-06-29 19:32:35 +0700

    Fix Router to display the pages correctly

--- src/components/Navbar/NavLink.jsx
+++ src/components/Navbar/NavLink.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import { Link, useColorModeValue } from "@chakra-ui/react";
 import { Link as RouteLink } from "react-router-dom";
 
-const NavLink = ({ name, to }) => (
-	<Link
-		as={RouteLink}
-		p="4px"
-		m="4px"
-		rounded={"md"}
-		fontWeight="500"
-		_hover={{
-			textDecoration: "none",
-			color: "black",
-			bg: useColorModeValue("gray.100", "gray.700"),
-		}}
-		to={to}
-	>
-		{name}
-	</Link>
-);
+const NavLink = ({ name, to }) => {
+	return (
+		<Link
+			as={RouteLink}
+			p="4px"
+			m="4px"
+			rounded={"md"}
+			fontWeight="500"
+			_hover={{
+				textDecoration: "none",
+				color: "black",
+				bg: useColorModeValue("gray.100", "gray.700"),
+			}}
+			to={to}
+		>
+			{name}
+		</Link>
+	);
+};
 
 export default NavLink;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,79 +14,78 @@ import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 import Logo from "../../logo.svg";
 import NavLink from "./NavLink";
 import NavData from "./NavData";
-import { BrowserRouter as Router } from "react-router-dom";
 import ProfileBar from "../Profilebar";
 
 const Navbar = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
-		<Router>
-			<Box
-				as="nav"
-				top="0"
-				pos="fixed"
-				zIndex="200"
-				w="100%"
-				pt="12px"
-				pb="12px"
-				boxShadow={"sm"}
-				bg={useColorModeValue("white", "blue")}
-				backdropFilter="saturate(180%) blur(24px)"
-			>
-				<Flex>
-					<Flex
-						ml={{ base: "12px", md: "48px" }}
-						mr={{ base: "12px", md: "48px" }}
-						w="100%"
-						alignItems={"center"}
-						justifyContent={"space-between"}
-					>
-						<IconButton
-							size={"md"}
-							icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-							aria-label={"Open Menu"}
-							display={{ md: "none" }}
-							onClick={isOpen ? onClose : onOpen}
-							transition="all 0.3s"
-						/>
-						<HStack spacing={8} alignItems={"center"}>
-							<HStack display={{ base: "none", md: "flex" }}>
-								<Image src={Logo} />
-								<Heading as={"h1"} fontSize={"2xl"}>
-									Hello Day!
-								</Heading>
-							</HStack>
-							<HStack
-								as={"nav"}
-								spacing={4}
-								display={{ base: "none", md: "flex" }}
-							>
-								{NavData.map((data) => (
-									<NavLink key={data.id} to={data.to}>
-										{data.name}
-									</NavLink>
-								))}
-							</HStack>
+		<Box
+			as="nav"
+			top="0"
+			pos="fixed"
+			zIndex="200"
+			w="100%"
+			pt="12px"
+			pb="12px"
+			boxShadow={"sm"}
+			bg={useColorModeValue("white", "blue")}
+			backdropFilter="saturate(180%) blur(24px)"
+		>
+			<Flex>
+				<Flex
+					ml={{ base: "12px", md: "48px" }}
+					mr={{ base: "12px", md: "48px" }}
+					w="100%"
+					alignItems={"center"}
+					justifyContent={"space-between"}
+				>
+					<IconButton
+						size={"md"}
+						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+						aria-label={"Open Menu"}
+						display={{ md: "none" }}
+						onClick={isOpen ? onClose : onOpen}
+						transition="all 0.3s"
+					/>
+					<HStack spacing={8} alignItems={"center"}>
+						<HStack display={{ base: "none", md: "flex" }}>
+							<Image src={Logo} />
+							<Heading as={"h1"} fontSize={"2xl"}>
+								Hello Day!
+							</Heading>
 						</HStack>
-						<Flex gap="2" alignItems={"center"}>
-							<ProfileBar name="Username" />
-						</Flex>
+						<HStack
+							as={"nav"}
+							spacing={4}
+							display={{ base: "none", md: "flex" }}
+						>
+							{NavData.map((data) => (
+								<NavLink
+									key={data.id}
+									to={data.to}
+									name={data.name}
+								></NavLink>
+							))}
+						</HStack>
+					</HStack>
+					<Flex gap="2" alignItems={"center"}>
+						<ProfileBar name="Username" />
 					</Flex>
 				</Flex>
-				{isOpen ? (
-					<Flex m="12px" p="4px" display={{ md: "none" }}>
-						<Stack as={"nav"} spacing={4}>
-							{NavData.map((data) => (
-								<NavLink key={data.id} to={data.to}>
-									{data.name}
-								</NavLink>
-							))}
-						</Stack>
-					</Flex>
-				) : null}
-			</Box>
-		</Router>
+			</Flex>
+			{isOpen ? (
+				<Flex m="12px" p="4px" display={{ md: "none" }}>
+					<Stack as={"nav"} spacing={4}>
+						{NavData.map((data) => (
+							<NavLink key={data.id} to={data.to}>
+								{data.name}
+							</NavLink>
+						))}
+					</Stack>
+				</Flex>
+			) : null}
+		</Box>
 	);
 };
 
