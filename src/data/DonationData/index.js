65a99b4c277941fd770623f151d3cf18e7915e1b
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-01 17:26:38 +0700

    Update Donation Data

--- src/data/DonationData/index.js
+++ src/data/DonationData/index.js
@@ -1,31 +1,34 @@
 const donationData = [
-  {
-    imgsrc:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80",
-    alt: "profile-logo",
-    name: "Anonymous",
-    nominal: "50.000",
-    time: "2",
-    message: "Semoga bisa membantu",
-  },
-  {
-    imgsrc:
-      "https://elireview.com/wp-content/uploads/2016/12/reed-profile-square.jpg",
-    alt: "profile-logo2",
-    name: "Anonymous",
-    nominal: "300.000",
-    time: "5",
-    message: "Semoga tidak ada korban jiwa",
-  },
-  {
-    imgsrc:
-      "https://cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png",
-    alt: "profile-logo2",
-    name: "Anonymous",
-    nominal: "160.000",
-    time: "14",
-    message: "Cuma sedikit namun saya harap bisa membantu teman-teman",
-  },
+	{
+		id: 1,
+		imgsrc:
+			"https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80",
+		alt: "profile-logo",
+		name: "Jonathan Wijaya",
+		nominal: "50.000",
+		time: "2",
+		message: "Semoga bisa membantu",
+	},
+	{
+		id: 2,
+		imgsrc:
+			"https://elireview.com/wp-content/uploads/2016/12/reed-profile-square.jpg",
+		alt: "profile-logo2",
+		name: "Anggyi Kusuma",
+		nominal: "300.000",
+		time: "5",
+		message: "Semoga tidak ada korban jiwa",
+	},
+	{
+		id: 3,
+		imgsrc:
+			"https://cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png",
+		alt: "profile-logo2",
+		name: "John Doe",
+		nominal: "160.000",
+		time: "14",
+		message: "Cuma sedikit namun saya harap bisa membantu teman-teman",
+	},
 ];
 
 export default donationData;
