0356b76a41773c922f466ba558b4b1fa8dad0235
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-01 20:14:52 +0700

    Refactor Code in Donation Card and Fundraising Content Pages

--- src/components/Cards/DonationCard/index.jsx
+++ src/components/Cards/DonationCard/index.jsx
@@ -1,46 +1,36 @@
 import React from "react";
-import donationData from "../../../data/DonationData";
-import { Box, HStack, Text, Image, VStack, Button } from "@chakra-ui/react";
+import { Box, HStack, Text, Image, VStack } from "@chakra-ui/react";
 
-const DonationCard = () => {
-  return (
-    <>
-      {donationData.map((donation) => {
-        return (
-          <Box boxShadow="md" p="6" rounded="md" bg="white" mb="5">
-            <VStack align="left">
-              <HStack>
-                <Image
-                  src={donation.imgsrc}
-                  alt={donation.alt}
-                  boxSize="80px"
-                  borderRadius="full"
-                  mr="5"
-                  mb="5"
-                />
-                <VStack align="left" spacing="5px">
-                  <Text fontWeight="bold" color="#FFA800">
-                    {donation.name}
-                  </Text>
-                  <HStack>
-                    <Text>Donated Rp</Text>
-                    <Text fontWeight="bold">{donation.nominal}</Text>
-                  </HStack>
-                  <Text fontSize="sm">{donation.time} days ago</Text>
-                </VStack>
-              </HStack>
-              <Text>{donation.message}</Text>
-            </VStack>
-          </Box>
-        );
-      })}
-      <Box align="center">
-        <Button colorScheme="yellow" variant="outline" w="200px" mt="5">
-          See More
-        </Button>
-      </Box>
-    </>
-  );
+const DonationCard = ({ image, alt, name, nominal, time, message }) => {
+	return (
+		<>
+			<Box boxShadow="sm" p="6" rounded="md" bg="white" mb="5">
+				<VStack align="left">
+					<HStack>
+						<Image
+							src={image}
+							alt={alt}
+							boxSize="80px"
+							borderRadius="full"
+							mr="5"
+							mb="5"
+						/>
+						<VStack align="left" spacing="5px">
+							<Text fontWeight="bold" color="#FFA800">
+								{name}
+							</Text>
+							<HStack>
+								<Text>Donated Rp</Text>
+								<Text fontWeight="bold">{nominal}</Text>
+							</HStack>
+							<Text fontSize="sm">{time} days ago</Text>
+						</VStack>
+					</HStack>
+					<Text>{message}</Text>
+				</VStack>
+			</Box>
+		</>
+	);
 };
 
 export default DonationCard;
