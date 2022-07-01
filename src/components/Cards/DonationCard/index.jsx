fb91b2191581f0e2675c8b8ffc2a7d36d6edb528
Wanda Hamidah <wandahmdh26@gmail.com>
2022-07-01 14:30:32 +0800

    Added Donation Card

---
+++ src/components/Cards/DonationCard/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import donationData from "../../../data/DonationData";
+import { Box, HStack, Text, Image, VStack } from "@chakra-ui/react";
+
+const DonationCard = () => {
+  return (
+    <>
+      {donationData.map((donation) => {
+        return (
+          <Box boxShadow="md" p="6" rounded="md" bg="white" mb="5">
+            <VStack align="left">
+              <HStack>
+                <Image
+                  src={donation.imgsrc}
+                  alt={donation.alt}
+                  boxSize="80px"
+                  borderRadius="full"
+                  mr="5"
+                  mb="5"
+                />
+                <VStack align="left" spacing="5px">
+                  <Text fontWeight="bold" color="#FFA800">
+                    {donation.name}
+                  </Text>
+                  <HStack>
+                    <Text>Donated Rp</Text>
+                    <Text fontWeight="bold">{donation.nominal}</Text>
+                  </HStack>
+                  <Text fontSize="sm">{donation.time} days ago</Text>
+                </VStack>
+              </HStack>
+              <Text>{donation.message}</Text>
+            </VStack>
+          </Box>
+        );
+      })}
+    </>
+  );
+};
+
+export default DonationCard;
