02aa173be8f551e465590a21debcc993db678bc4
GuestBrutal <kmbrps@gmail.com>
2022-07-03 04:08:07 +0700

    Add News in Volunteer Page

--- src/data/VolunteerNewsData/index.js
+++ src/data/VolunteerNewsData/index.js
@@ -15,36 +15,37 @@ const VolunteerNewsData = [
 	{
 		id: 2,
 		imageUrl:
-			"https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
+			"https://c.ndtvimg.com/2021-12/hpin06d8_indonesia_625x300_10_December_21.jpg",
 		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
-		title: "Help our friend on Nusa Tenggara Timur",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+		title:
+			"No Warning: Indonesian Village Caught In Volcano Disaster That Killed 34",
+		desc: "In the wake of the deadly Semeru eruption on Indonesia's Java island, the entwined bodies of a mother and daughter encased in molten ash have come to symbolise what many living in the shadow of the volcano feel went wrong.",
 		editor: "Kompas News",
 		link: "#",
 	},
 	{
 		id: 3,
 		imageUrl:
-			"https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
+			"https://c.ndtvimg.com/2021-11/1833ov5k_indonesia-flash-flood_625x300_05_November_21.jpg",
 		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
-		title: "Help our friend on South Kalimantan",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+		title: "At Least 8 Killed, 3 Still Missing In Flash Floods In Indonesia",
+		desc: "Indonesian rescuers scrambled Friday to find survivors under mud-swamped hillsides after flash floods on Java island killed at least eight people, the disaster agency said.",
 		editor: "Kompas News",
 		link: "#",
 	},
 	{
 		id: 4,
 		imageUrl:
-			"https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
+			"https://c.ndtvimg.com/2021-04/3tjc0k3_indonesia-earthquake_625x300_10_April_21.jpg",
 		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
-		title: "Help our friend on Nusa Tenggara Timur",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+		title: "Earthquake Death Count Rises To 7 In Indonesia",
+		desc: "At least seven people were killed after a 6.0 magnitude quake struck off the coast of Indonesia's main Java island Saturday, as the country reels from a cyclone disaster in another part of the archipelago",
 		editor: "Kompas News",
 		link: "#",
 	},
