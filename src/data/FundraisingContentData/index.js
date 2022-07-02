162312fa8ddea32aaf93190cefb398fdc6dcaa01
Wanda Hamidah <wandahmdh26@gmail.com>
2022-07-02 23:15:06 +0800

    Added data for Fundraising Menu Components

---
+++ src/data/FundraisingContentData/index.js
@@ -0,0 +1,94 @@
+const FundraisingContentData = [
+  {
+    id: 1,
+    imageUrl:
+      "https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
+    imageAlt: "donate card",
+    imageBy:
+      "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
+    title: "Help our friend on South Kalimantan",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    progress: 79,
+    achieved: "Rp 15.920.000",
+    target: "Rp 20.000.000",
+    target2: "Rp 100.000.000",
+    link: "#",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
+    imageAlt: "donate card",
+    imageBy:
+      "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
+    title: "Help our friend on Nusa Tenggara Timur",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    progress: 36,
+    achieved: "Rp 36.800.000",
+    target: "Rp 100.000.000",
+    target2: "Rp 100.000.000",
+    link: "#",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
+    imageAlt: "donate card",
+    imageBy:
+      "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
+    title: "Help our friend on South Kalimantan",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    progress: 79,
+    achieved: "Rp 15.920.000",
+    target: "Rp 20.000.000",
+    target2: "Rp 100.000.000",
+    link: "#",
+  },
+  {
+    id: 4,
+    imageUrl:
+      "https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
+    imageAlt: "donate card",
+    imageBy:
+      "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
+    title: "Help our friend on Nusa Tenggara Timur",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    progress: 36,
+    achieved: "Rp 36.800.000",
+    target: "Rp 100.000.000",
+    target2: "Rp 100.000.000",
+    link: "#",
+  },
+  {
+    id: 5,
+    imageUrl:
+      "https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
+    imageAlt: "donate card",
+    imageBy:
+      "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
+    title: "Help our friend on South Kalimantan",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    progress: 79,
+    achieved: "Rp 15.920.000",
+    target: "Rp 20.000.000",
+    target2: "Rp 100.000.000",
+    link: "#",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
+    imageAlt: "donate card",
+    imageBy:
+      "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
+    title: "Help our friend on Nusa Tenggara Timur",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    progress: 36,
+    achieved: "Rp 36.800.000",
+    target: "Rp 100.000.000",
+    target2: "Rp 100.000.000",
+    link: "#",
+  },
+];
+
+export default FundraisingContentData;
