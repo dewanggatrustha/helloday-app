84568bb9aa6093c2afbcd53de7b45740417f113f
GuestBrutal <kmbrps@gmail.com>
2022-07-03 04:03:02 +0700

    Add News in Volunteer Page

--- src/data/VolunteerNewsData/index.js
+++ src/data/VolunteerNewsData/index.js
@@ -2,79 +2,74 @@ const VolunteerNewsData = [
 	{
 		id: 1,
 		imageUrl:
-			"https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
-		imageAlt: "volunteer card",
+			"https://c.ndtvimg.com/2022-03/76m1qtpg_merapi-volcano-afp_625x300_10_March_22.jpg?im=FaceCrop,algorithm=dnn,width=620,height=350",
+		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
-		title: "Bantu Korban Tsunami di Aceh",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+		title:
+			"Indonesia's Merapi Spews Out Volcanic Lava And Ash, Hundreds Evacuate",
+		desc: "Indonesia's Merapi volcano erupted overnight, sending hot lava and ash down its slopes and prompting over 250 residents in the surrounding area to evacuate, the country's disaster mitigation agency (BNPB) said on Thursday.",
 		editor: "Kompas News",
-		achieved: "5",
 		link: "#",
 	},
 	{
 		id: 2,
 		imageUrl:
 			"https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
-		imageAlt: "volunteer card",
+		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
 		title: "Help our friend on Nusa Tenggara Timur",
 		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
 		editor: "Kompas News",
-		achieved: "5",
 		link: "#",
 	},
 	{
 		id: 3,
 		imageUrl:
 			"https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
-		imageAlt: "volunteer card",
+		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
 		title: "Help our friend on South Kalimantan",
 		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
 		editor: "Kompas News",
-		achieved: "5",
 		link: "#",
 	},
 	{
 		id: 4,
 		imageUrl:
 			"https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
-		imageAlt: "volunteer card",
+		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
 		title: "Help our friend on Nusa Tenggara Timur",
 		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
 		editor: "Kompas News",
-		achieved: "5",
 		link: "#",
 	},
 	{
 		id: 5,
 		imageUrl:
 			"https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
-		imageAlt: "volunteer card",
+		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
 		title: "Help our friend on South Kalimantan",
 		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
 		editor: "Kompas News",
-		achieved: "5",
 		link: "#",
 	},
 	{
 		id: 6,
 		imageUrl:
 			"https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
-		imageAlt: "volunteer card",
+		imageAlt: "news card",
 		imageBy:
 			"https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
 		title: "Help our friend on Nusa Tenggara Timur",
 		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
 		editor: "Kompas News",
-		achieved: "5",
 		link: "#",
 	},
 ];
