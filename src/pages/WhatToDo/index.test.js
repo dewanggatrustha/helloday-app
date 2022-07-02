81474b9d4cb59920eb6ee78cb5b2db1a5addaebf
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-02 15:21:38 +0700

    Changes in WhatToDo Pages

--- src/pages/WhatToDo/index.test.js
+++ src/pages/WhatToDo/index.test.js
@@ -1,41 +1,51 @@
 import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import WhatToDo from "./index";
 
-const data = {
-  disaster: {
-    name: "Earthquake",
-    definition:
-      "Earthquake is a term used to describe both sudden slip on a fault, and the resulting ground shaking and radiated seismic energy caused by the slip, or by volcanic or magmatic activity, or other sudden stress changes in the earth.",
-    image: [
-      "https://cdn.britannica.com/34/127134-050-49EC55CD/Building-foundation-earthquake-Japan-Kobe-January-1995.jpg",
-      "https://static01.nyt.com/images/2021/08/14/world/14haiti-earthquake-live-us-diaspora1/14haiti-earthquake-live-us-diaspora1-videoSixteenByNine3000.jpg",
-      "https://images.newscientist.com/wp-content/uploads/2015/10/mg22830412.800-1_800.jpg?width=800",
-    ],
-  },
-};
-
 const testRender = () =>
-  render(
-    <>
-      <WhatToDo
-        name={data.disaster.name}
-        definition={data.disaster.definition}
-        image={data.disaster.image}
-      />
-    </>
-  );
+	render(
+		<>
+			<WhatToDo />
+		</>
+	);
 
-describe("Render Donation Card Component", () => {
-  beforeEach(testRender);
-  afterEach(cleanup);
+describe("Render What to Do Pages", () => {
+	beforeEach(testRender);
+	afterEach(cleanup);
 
-  it("Component Rendered Successfully", () => {
-    // const image = screen.getByText(data.disaster.image);
-    const name = screen.getByText(data.disaster.name);
-    const definition = screen.getByText(data.disaster.definition);
+	it("Tab Component Rendered Successfully", () => {
+		const InstructionTab = screen.getByText(/Instruction/i);
+		const emergencyCallsTab = screen.getByText(/Emergency Calls/i);
 
-    // expect(image).toBeInTheDocument();
-    expect(name).toBeInTheDocument();
-    expect(definition).toBeInTheDocument();
-  });
+		userEvent.click(emergencyCallsTab);
+
+		expect(InstructionTab).toBeInTheDocument();
+		expect(emergencyCallsTab).toBeInTheDocument();
+	});
+
+	it("Accordion Component Rendered Successfully", () => {
+		const disasterTitle = screen.getByText(/Earthquake/i);
+		const disasterDefinition = screen.getByText(
+			/Earthquake is a term used to describe both sudden slip on a fault, and the resulting ground shaking and radiated seismic energy caused by the slip, or by volcanic or magmatic activity, or other sudden stress changes in the earth./i
+		);
+		const instructionTitle = screen.getByText(/Indoor/i);
+		const instructionStep = screen.getByText(
+			/Electronic devices are experiencing strange interference at home./i
+		);
+
+		expect(disasterTitle).toBeInTheDocument();
+		expect(disasterDefinition).toBeInTheDocument();
+		expect(instructionTitle).toBeInTheDocument();
+		expect(instructionStep).toBeInTheDocument();
+	});
+
+	it("Emergency Card Component Rendered Successfully", () => {
+		const image = screen.getAllByAltText(/Ambulance/i);
+		const name = screen.getByText(/Ambulance/i);
+		const number = screen.getByText(/119/i);
+
+		expect(image).toBeTruthy();
+		expect(name).toBeInTheDocument();
+		expect(number).toBeInTheDocument();
+	});
 });
