c600692cd31bd667bec91fa48599886f0bc8e86a
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-02 17:28:52 +0700

    Convert data to TypeScript

---
+++ src/data/InstructionData/index.ts
@@ -0,0 +1,21 @@
+import { earthquakeInformation, earthquakeInstruction } from "./earthquakeData";
+import { fireInformation, fireInstruction } from "./fireData";
+import { floodInformation, floodInstruction } from "./floodData";
+import { landSlideInformation, landSlideInstruction } from "./landSlideData";
+import { tornadoInformation, tornadoInstruction } from "./tornadoData";
+import { tsunamiInformation, tsunamiInstruction } from "./tsunamiData";
+
+export {
+	earthquakeInformation,
+	earthquakeInstruction,
+	fireInformation,
+	fireInstruction,
+	floodInformation,
+	floodInstruction,
+	landSlideInformation,
+	landSlideInstruction,
+	tornadoInformation,
+	tornadoInstruction,
+	tsunamiInformation,
+	tsunamiInstruction,
+};
