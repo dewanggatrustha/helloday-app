4928828abbc9ca49db93f79fcb370e218b4d924d
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-03 00:04:45 +0700

    Add Content to DisasterMap Pages

--- src/pages/DisasterMap/index.jsx
+++ src/pages/DisasterMap/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {
 	Box,
+	Flex,
 	Heading,
 	Divider,
 	Stack,
 	VStack,
 	useColorModeValue,
 } from "@chakra-ui/react";
+import HelloMap from "../../components/HelloMap";
+import LatestDisasterCard from "../../components/Cards/DisasterMap/LatestDisasterCard";
 
 const DisasterMap = () => {
 	return (
@@ -15,7 +18,7 @@ const DisasterMap = () => {
 				m={{ base: "88px 12px 0px 12px", md: "88px 48px 0px 48px" }}
 				bg={useColorModeValue("white", "blue")}
 			>
-				<Stack direction="row" h="30px">
+				<Stack direction="row" h="30px" mb="14px">
 					<Divider
 						orientation="vertical"
 						borderWidth="4px"
@@ -23,16 +26,22 @@ const DisasterMap = () => {
 						borderColor="primary.500"
 					/>
 					<VStack align="left">
-						<Heading as="h4" size="xl">
+						<Heading as="h2" size="xl">
 							Disaster Map
-						</Heading>
-						<Heading as="h3" size="md" fontWeight="500">
-							View latest information about natural disaster that
-							happened in the world especially in Indonesia!
 						</Heading>
 					</VStack>
 				</Stack>
-				{/* Ngoding Disini */}
+				<Heading as="h3" size="md" fontWeight="500">
+					View latest information about natural disaster that happened in
+					the world especially in Indonesia!
+				</Heading>
+				<Divider mt="24px" mb="24px" />
+				<Flex>
+					<LatestDisasterCard />
+					<Flex>
+						<HelloMap />
+					</Flex>
+				</Flex>
 			</Box>
 		</>
 	);
