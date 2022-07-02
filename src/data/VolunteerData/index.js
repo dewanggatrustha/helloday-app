9f7dc7b071c89d867d5194e724409d8c7a9f32c4
Muhamad Ivan Fadilah <muhammadivan174@gmail.com>
2022-07-02 19:44:24 +0700

    Add required data for Volunteer Pages

---
+++ src/data/VolunteerData/index.js
@@ -0,0 +1,28 @@
+const volunteerData = [
+  {
+    id: 1,
+    imgsrc:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80",
+    alt: "profile-logo",
+    name: "Jonathan Wijaya",
+    time: "2",
+  },
+  {
+    id: 2,
+    imgsrc:
+      "https://elireview.com/wp-content/uploads/2016/12/reed-profile-square.jpg",
+    alt: "profile-logo2",
+    name: "Anggyi Kusuma",
+    time: "5",
+  },
+  {
+    id: 3,
+    imgsrc:
+      "https://cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png",
+    alt: "profile-logo2",
+    name: "John Doe",
+    time: "14",
+  },
+];
+
+export default volunteerData;
