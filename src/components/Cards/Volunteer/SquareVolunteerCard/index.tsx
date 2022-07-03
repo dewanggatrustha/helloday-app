42bf56aedab94926e815bf4dc07908272a3f33db
Wanda Hamidah <wandahmdh26@gmail.com>
2022-07-03 23:40:54 +0800

    Fix layout for Square Volunteer Card

--- src/components/Cards/Volunteer/SquareVolunteerCard/index.tsx
+++ src/components/Cards/Volunteer/SquareVolunteerCard/index.tsx
@@ -1,105 +1,81 @@
 import React from "react";
 import {
-	Box,
-	Image,
-	HStack,
-	Text,
-	VStack,
-	LinkBox,
-	LinkOverlay,
-	Grid,
-	GridItem,
+  Box,
+  Image,
+  HStack,
+  Text,
+  VStack,
+  LinkBox,
+  LinkOverlay,
+  Divider,
 } from "@chakra-ui/react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
 
 interface SquareVolunteerCardProps {
-	imageAlt: string;
-	imageUrl: string;
-	title: string;
-	desc: string;
-	koordinator: string;
-	achieved: string;
-	link: string;
+  imageAlt: string;
+  imageUrl: string;
+  title: string;
+  desc: string;
+  link: string;
+  imgOrg: string;
+  organizer: string;
 }
 const SquareVolunteerCard = ({
-	imageAlt,
-	imageUrl,
-	title,
-	desc,
-	koordinator,
-	achieved,
-	link,
+  imageAlt,
+  imageUrl,
+  title,
+  desc,
+  link,
+  imgOrg,
+  organizer,
 }: SquareVolunteerCardProps) => {
-	return (
-		<LinkBox
-			border="1px"
-			borderColor="gray.100"
-			borderRadius="10px"
-			boxSize="350px"
-			overflow="hidden"
-			// align= 'right' as 'right'
-			boxShadow="md"
-			ml="2"
-			mr="5"
-			p="5"
-			mb="2"
-		>
-			<Box>
-				<VStack>
-					<Image
-						w="300px"
-						h="120"
-						objectFit="cover"
-						src={imageUrl}
-						alt={imageAlt}
-						borderRadius="base"
-					/>
-				</VStack>
-				<HStack spacing="24px" mt="2">
-					<VStack spacing="5px">
-						<Box w="100%">
-							<LinkOverlay href={link} fontWeight="bold" fontSize={14} mb="3">
-								{title}
-							</LinkOverlay>
-							<Text align="left" fontSize={12} mb="2">
-								{desc}
-							</Text>
-						</Box>
-						<Box w="100%">
-							<Grid templateColumns="repeat(6, 1fr)">
-								<GridItem colEnd={2}>
-									<Image
-										w={7}
-										h={7}
-										src="http://logistik.bnpb.go.id/inalogpb/assets/img/Logo%20bnpb-01.png"
-									/>
-								</GridItem>
-								<GridItem colStart={2} colEnd={6}>
-									<Text>{koordinator}</Text>
-								</GridItem>
-							</Grid>
-							<HStack mt="2" fontSize={12}>
-								<Text>{achieved} Relawan Telah Bergabung disini</Text>
-							</HStack>
-						</Box>
-						<Link to="/register">
-							<Box
-								boxShadow="xl"
-								as="button"
-								borderRadius="md"
-								bg="#F4CC3E"
-								color="white"
-								px={4}
-								h={8}
-							>
-								Bergabung Jadi Relawan
-							</Box>
-						</Link>
-					</VStack>
-				</HStack>
-			</Box>
-		</LinkBox>
-	);
+  return (
+    <LinkBox
+      border="1px"
+      borderColor="gray.100"
+      borderRadius="10px"
+      boxSize="440px"
+      overflow="hidden"
+      boxShadow="md"
+      ml="2"
+      mr="5"
+      p="5"
+      mb="2"
+    >
+      <Box>
+        <VStack>
+          <Image
+            w="400px"
+            h="200px"
+            objectFit="cover"
+            src={imageUrl}
+            alt={imageAlt}
+            borderRadius="base"
+            mb="2"
+          />
+        </VStack>
+        <HStack spacing="24px" mt="2">
+          <VStack spacing="5px">
+            <Box w="100%">
+              <LinkOverlay href={link} fontWeight="bold" fontSize={18} mb="5">
+                {title}
+              </LinkOverlay>
+              <Text align="left" fontSize={13} mb="2" mt="3">
+                {desc}
+              </Text>
+            </Box>
+            <Divider />
+            <Box w="100%">
+              <Text fontWeight="600">Created by</Text>
+              <HStack mt="2" mb="2" fontSize={13}>
+                <Image src={imgOrg} boxSize="30px" borderRadius="full" />
+                <Text>{organizer}</Text>
+              </HStack>
+            </Box>
+          </VStack>
+        </HStack>
+      </Box>
+    </LinkBox>
+  );
 };
 
 export default SquareVolunteerCard;
