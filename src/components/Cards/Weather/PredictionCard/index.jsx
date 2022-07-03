72c8a0884f87c3d48b752a5239632dfd4f09caf7
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-03 21:30:57 +0700

    Changes Layout in Weather Pages

--- src/components/Cards/Weather/PredictionCard/index.jsx
+++ src/components/Cards/Weather/PredictionCard/index.jsx
@@ -7,56 +7,59 @@ import WeeklyCard from "../WeeklyCard";
 import WeeklyWeatherData from "../../../../data/WeeklyWeatherData";
 
 const PredictionCard = () => {
-  return (
-    <Flex
-      bg="white"
-      w="lg"
-      h="100%"
-      boxShadow="md"
-      p="48px"
-      rounded="15px"
-      flexDirection="column"
-    >
-      <Heading as="h5" fontSize="xl">
-        This Week
-      </Heading>
-      <VStack align="left">
-        <Heading>Today</Heading>
-        <Box mt="5" mb="10" p="1">
-          <HStack align="left">
-            <Swiper
-              spaceBetween={20}
-              slidesPerView={4}
-              onSlideChange={() => console.log("slide change")}
-              onSwiper={(swiper) => console.log(swiper)}
-            >
-              {WeatherData.map((weather) => {
-                return (
-                  <SwiperSlide>
-                    <ConditionCard time={weather.time} temp={weather.temp} />
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </HStack>
-          <VStack>
-            {WeeklyWeatherData.slice(0, 4).map((weekly) => {
-              return (
-                <SwiperSlide>
-                  <WeeklyCard
-                    day={weekly.day}
-                    date={weekly.date}
-                    temp={weekly.temp}
-                    icon={weekly.icon}
-                  />
-                </SwiperSlide>
-              );
-            })}
-          </VStack>
-        </Box>
-      </VStack>
-    </Flex>
-  );
+	return (
+		<Flex
+			bg="white"
+			w="lg"
+			h="100%"
+			p={"20px 48px 20px 48px"}
+			rounded="15px"
+			flexDirection="column"
+			boxShadow="md"
+		>
+			<Heading as="h5" fontSize="xl">
+				This Week
+			</Heading>
+			<VStack align="left">
+				<Heading>Today</Heading>
+				<Box mt="5" mb="10" p="1">
+					<HStack align="left">
+						<Swiper
+							spaceBetween={20}
+							slidesPerView={4}
+							onSlideChange={() => console.log("slide change")}
+							onSwiper={(swiper) => console.log(swiper)}
+						>
+							{WeatherData.map((weather) => {
+								return (
+									<SwiperSlide>
+										<ConditionCard
+											time={weather.time}
+											temp={weather.temp}
+										/>
+									</SwiperSlide>
+								);
+							})}
+						</Swiper>
+					</HStack>
+					<VStack>
+						{WeeklyWeatherData.slice(0, 4).map((weekly) => {
+							return (
+								<SwiperSlide>
+									<WeeklyCard
+										day={weekly.day}
+										date={weekly.date}
+										temp={weekly.temp}
+										icon={weekly.icon}
+									/>
+								</SwiperSlide>
+							);
+						})}
+					</VStack>
+				</Box>
+			</VStack>
+		</Flex>
+	);
 };
 
 export default PredictionCard;
