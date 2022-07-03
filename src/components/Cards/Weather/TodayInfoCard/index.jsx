6f7eec42907fb324f38574854b4357849d06c047
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-03 23:36:38 +0700

    Changes to Today Info Card Component

--- src/components/Cards/Weather/TodayInfoCard/index.jsx
+++ src/components/Cards/Weather/TodayInfoCard/index.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { Box, Flex, Heading, Text, HStack } from "@chakra-ui/react";
+import {
+	Box,
+	Flex,
+	Heading,
+	Text,
+	HStack,
+	VStack,
+	Image,
+} from "@chakra-ui/react";
 import { RiMapPinLine } from "react-icons/ri";
+import { TbTemperature } from "react-icons/tb";
+import { WiCloud } from "react-icons/wi";
+import { MdOutlineTipsAndUpdates } from "react-icons/md";
+import TemperatureStats from "../../../../assets/weather/Card/TemperatureStats.svg";
 
 const TodayInfoCard = () => {
 	return (
 		<Flex
-			bgImage="https://images.unsplash.com/photo-1612251276789-9b1a8f2add8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+			bgImage="https://images.unsplash.com/photo-1597200381847-30ec200eeb9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 			bgSize="cover"
 			w="full"
 			h="xs"
@@ -17,21 +29,36 @@ const TodayInfoCard = () => {
 			<Flex flexDirection="column" w="50%" justifyContent="space-between">
 				<HStack>
 					<RiMapPinLine size="28px" />
-					<Heading as="h3" fontWeight="600" fontSize="xl">
-						Jakarta, Indonesia
+					<Heading as="h3" fontWeight="500" fontSize="xl">
+						DKI Jakarta, Indonesia
 					</Heading>
 				</HStack>
-				<Flex verticalAlign="top">
-					<Heading as="h4" fontSize="7xl" fontWeight="500">
-						26
-					</Heading>
-					<Heading as="h5" fontSize="3xl" fontWeight="600">
-						°C
+				<Flex flexDirection="column">
+					<Flex verticalAlign="top" pl="155px">
+						<Heading as="h4" fontSize="7xl" fontWeight="500">
+							26
+						</Heading>
+						<Heading as="h5" fontSize="3xl" fontWeight="500">
+							°C
+						</Heading>
+					</Flex>
+					<Heading as="h5" fontSize="xl" fontWeight="500" pl="145px">
+						Mostly Clear
 					</Heading>
 				</Flex>
 				<Flex>
-					<Text></Text>
-					<Text></Text>
+					<HStack mr="24px">
+						<TbTemperature size="22px" mt="2px" />
+						<Text fontSize="lg" fontWeight="500">
+							Feels like 28°C
+						</Text>
+					</HStack>
+					<HStack ml="24px">
+						<WiCloud size="22px" mt="2px" />
+						<Text fontSize="lg" fontWeight="500">
+							Cloudly - 0%
+						</Text>
+					</HStack>
 				</Flex>
 			</Flex>
 
@@ -45,13 +72,19 @@ const TodayInfoCard = () => {
 					textAlign="center"
 					filter="auto"
 					opacity="50%"
-					w="450px"
-					h="230px"
+					w="460px"
+					h="245px"
 					bg="white"
 					rounded="15px"
 				></Box>
-				<Flex zIndex="2" p="12px">
-					Test
+				<Flex zIndex="2" p={"12px 24px 12px 36px"} flexDirection="column">
+					<HStack>
+						<MdOutlineTipsAndUpdates mt="1px" />
+						<Text fontSize="lg" fontWeight="500">
+							Temperature of the Day
+						</Text>
+					</HStack>
+					<Image src={TemperatureStats} w="400px" />
 				</Flex>
 			</Flex>
 		</Flex>
