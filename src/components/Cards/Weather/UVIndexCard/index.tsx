93d78b4a67992b6822e56a7f215c5aa5c03809ca
Wanda Hamidah <wandahmdh26@gmail.com>
2022-07-03 21:57:29 +0800

    Modified Multiple Weather Components

--- src/components/Cards/Weather/UVIndexCard/index.tsx
+++ src/components/Cards/Weather/UVIndexCard/index.tsx
@@ -3,31 +3,31 @@ import { Flex, Heading, Text, Image } from "@chakra-ui/react";
 import UVIndexIcon from "../../../../assets/weather/Card/UVIndexIcon.png";
 
 const UVIndexCard = () => {
-	return (
-		<Flex
-			bg="white"
-			w="lg"
-			h="180px"
-			boxShadow="md"
-			p="48px"
-			justifyContent="space-around"
-			rounded="15px"
-		>
-			<Flex flexDirection="column" gap="4px">
-				<Heading as="h4" fontSize="2xl" fontWeight="600">
-					UV Index
-				</Heading>
-				<Text fontWeight="500">Today UV Index</Text>
-				<Heading as="h5" fontSize="2xl" fontWeight="600">
-					8.9
-				</Heading>
-			</Flex>
-			<Flex>
-				{" "}
-				<Image src={UVIndexIcon} />
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      bg="#EDF3F8"
+      w="lg"
+      h="180px"
+      boxShadow="md"
+      p="48px"
+      justifyContent="space-around"
+      rounded="15px"
+    >
+      <Flex flexDirection="column" gap="4px">
+        <Heading as="h4" fontSize="2xl" fontWeight="600">
+          UV Index
+        </Heading>
+        <Text fontWeight="500">Today UV Index</Text>
+        <Heading as="h5" fontSize="2xl" fontWeight="600">
+          8.9
+        </Heading>
+      </Flex>
+      <Flex>
+        {" "}
+        <Image src={UVIndexIcon} />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default UVIndexCard;
--- src/components/Cards/Weather/WindCard/index.tsx
+++ src/components/Cards/Weather/WindCard/index.tsx
@@ -3,30 +3,30 @@ import { Flex, Heading, Text, Image } from "@chakra-ui/react";
 import WindIcon from "../../../../assets/weather/Card/WindSpeedIcon.png";
 
 const WindCard = () => {
-	return (
-		<Flex
-			bg="white"
-			w="lg"
-			h="180px"
-			boxShadow="md"
-			p="48px"
-			justifyContent="space-around"
-			rounded="15px"
-		>
-			<Flex flexDirection="column" gap="4px">
-				<Heading as="h4" fontSize="2xl" fontWeight="600">
-					Wind Status
-				</Heading>
-				<Text fontWeight="500">Today Wind Speed</Text>
-				<Heading as="h5" fontSize="2xl" fontWeight="600">
-					5 km/h
-				</Heading>
-			</Flex>
-			<Flex>
-				<Image src={WindIcon} />
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      bg="#EDF3F8"
+      w="lg"
+      h="180px"
+      boxShadow="md"
+      p="48px"
+      justifyContent="space-around"
+      rounded="15px"
+    >
+      <Flex flexDirection="column" gap="4px">
+        <Heading as="h4" fontSize="2xl" fontWeight="600">
+          Wind Status
+        </Heading>
+        <Text fontWeight="500">Today Wind Speed</Text>
+        <Heading as="h5" fontSize="2xl" fontWeight="600">
+          5 km/h
+        </Heading>
+      </Flex>
+      <Flex>
+        <Image src={WindIcon} />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default WindCard;
