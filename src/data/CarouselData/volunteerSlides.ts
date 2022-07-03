617da000be159aa4a40ed1b80784c71f0dd76558
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-03 22:22:49 +0700

    Create Carousel Component for Volunteer Pages

---
+++ src/data/CarouselData/volunteerSlides.ts
@@ -0,0 +1,13 @@
+const volunteerSlides = [
+	{
+		img: "https://images.pexels.com/photos/2599537/pexels-photo-2599537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	},
+	{
+		img: "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	},
+	{
+		img: "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+	},
+];
+
+export default volunteerSlides;
