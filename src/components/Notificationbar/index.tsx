208a13da71d795990e7ecb9fa78fd8e6ba1fbf7f
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-07-03 21:39:44 +0700

    Changes to the Notificationbar Component

--- src/components/Notificationbar/index.tsx
+++ src/components/Notificationbar/index.tsx
@@ -7,6 +7,8 @@ import {
 	MenuList,
 	MenuItem,
 	MenuDivider,
+	Text,
+	VStack,
 } from "@chakra-ui/react";
 import { BellIcon } from "@chakra-ui/icons";
 
@@ -18,11 +20,25 @@ const Notificationbar = () => {
 			</MenuButton>
 			<MenuList>
 				<MenuItem>
-					<Box>Notification 1</Box>
+					<Box>
+						<VStack>
+							<Text fontWeight={600}>Earthquake - 5.2 SR</Text>
+							<Text fontSize="xs">Sunday, July 3, 2022 - 12.43 WIB</Text>
+							<Text fontSize="sm">Yogyakarta, DIY</Text>
+						</VStack>
+					</Box>
 				</MenuItem>
 				<MenuDivider />
 				<MenuItem>
-					<Box>Notification 1</Box>
+					<Box>
+						<VStack>
+							<Text fontWeight={600}>Tsunami - 40 Km</Text>
+							<Text fontSize="xs">
+								Tuesday, July 3, 2022 - 10.15 WIB
+							</Text>
+							<Text fontSize="sm">Pangandaran, West Java</Text>
+						</VStack>
+					</Box>
 				</MenuItem>
 			</MenuList>
 		</Menu>
