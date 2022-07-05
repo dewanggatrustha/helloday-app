178a278c936a2c516172ea480cbd4e4160f97ac7
Wanda Hamidah <wandahmdh26@gmail.com>
2022-07-05 21:20:19 +0800

    Change data for Volunteer Content Data

--- src/data/VolunteerContentData/index.ts
+++ src/data/VolunteerContentData/index.ts
@@ -7,7 +7,7 @@ const volunteercontentData = [
     imageBy:
       "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
     title: "Help our friend on South Kalimantan",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    desc: "The flood occurred in Hulu Sungai Tengah Regency, South Kalimantan Province (Kalsel), on Tuesday (11/1/2022), at 09.20 local time or WITA.",
     progress: 79,
     achieved: "Rp 15.920.000",
     target: "Rp 20.000.000",
@@ -26,7 +26,7 @@ const volunteercontentData = [
     imageBy:
       "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
     title: "Help our friend on Nusa Tenggara Timur",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    desc: "Flash floods hit East Flores Regency, East Nusa Tenggara (NTT) on Sunday early in the morning. The latest news says 54 people died in the incident.",
     progress: 36,
     achieved: "Rp 36.800.000",
     target: "Rp 100.000.000",
@@ -40,12 +40,12 @@ const volunteercontentData = [
   {
     id: 3,
     imageUrl:
-      "https://awsimages.detik.net.id/community/media/visual/2021/01/23/potret-kalimantan-selatan-setelah-sepekan-dilanda-banjir-besar-2_169.jpeg?w=700&q=90",
+      "https://asset.kompas.com/crops/anBjjGbAKGZ7ye5hLhx6iyLjC8U=/0x0:0x0/750x500/data/photo/2021/12/05/61ac286843df7.jpg",
     imageAlt: "donate card",
     imageBy:
       "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
-    title: "Help our friend on South Kalimantan",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    title: "Help victims of Mount Semeru Eruption",
+    desc: "Grief enveloped the people of East Java at the end of 2021. The highest mountain on the island of Java, Semeru, released a fairly large Abortion Hot Cloud.",
     progress: 79,
     achieved: "Rp 15.920.000",
     target: "Rp 20.000.000",
@@ -59,12 +59,12 @@ const volunteercontentData = [
   {
     id: 4,
     imageUrl:
-      "https://disk.mediaindonesia.com/thumbs/1800x1200/news/2021/04/0b20bc6c7b8dab5c99cb1d4e46f00f38.jpeg",
+      "https://imgsrv2.voi.id/98u8SQ7TVdWw4sSCsI5tkN0RRaQAwQC_tZxhz6-jZGg/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy84OTMyMC8yMDIxMDkyODE1MjMtbWFpbi5qcGVn.jpg",
     imageAlt: "donate card",
     imageBy:
       "https://upload.wikimedia.org/wikipedia/commons/5/57/UNICEF_Logo.png",
-    title: "Help our friend on Nusa Tenggara Timur",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+    title: "Help The Earthquake Victims in Palu",
+    desc: "On September 28, 2018, an earthquake rocked Donggala Regency and Palu City. After that, aftershocks appeared and triggered a tsunami...",
     progress: 36,
     achieved: "Rp 36.800.000",
     target: "Rp 100.000.000",
