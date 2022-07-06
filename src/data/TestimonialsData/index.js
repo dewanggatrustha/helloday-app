4ba2b6b850b202ab019c3a81751c76d9098b749d
Muhamad Ivan Fadilah <muhammadivan174@gmail.com>
2022-07-06 21:58:30 +0700

    Update data for Testimonial Data

--- src/data/TestimonialsData/index.js
+++ src/data/TestimonialsData/index.js
@@ -2,36 +2,36 @@ const TestimonialsData = [
   {
     id: 1,
     imgProfile:
-      "https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
     imageAlt: "Profile Img",
-    name: "Unknown",
+    name: "Howard Wolowitz",
     testi:
       "HelloDay has a user-freandly plaform and high volunteer traffic. Thank you HelloDay for helping us to search an engange with people eho shae the same vision as us.",
   },
   {
     id: 2,
     imgProfile:
-      "https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
     imageAlt: "Profile Img",
-    name: "Unknown",
+    name: "Sheldon Cooper",
     testi:
       "HelloDay has a user-freandly plaform and high volunteer traffic. Thank you HelloDay for helping us to search an engange with people eho shae the same vision as us.",
   },
   {
     id: 3,
     imgProfile:
-      "https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
+      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
     imageAlt: "Profile Img",
-    name: "Unknown",
+    name: "Leonard Hofstadter",
     testi:
       "HelloDay has a user-freandly plaform and high volunteer traffic. Thank you HelloDay for helping us to search an engange with people eho shae the same vision as us.",
   },
   {
     id: 4,
     imgProfile:
-      "https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
+      "https://images.unsplash.com/photo-1569779213435-ba3167dde7cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
     imageAlt: "Profile Img",
-    name: "Unknown",
+    name: "Jim Parsons",
     testi:
       "HelloDay has a user-freandly plaform and high volunteer traffic. Thank you HelloDay for helping us to search an engange with people eho shae the same vision as us.",
   },
