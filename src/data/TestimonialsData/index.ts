2cfc25d12ad452631bdf241a6d1cdc01a74deda9
Muhamad Ivan Fadilah <muhammadivan174@gmail.com>
2022-07-06 22:11:24 +0700

    Update data for Testimonials Data

--- src/data/TestimonialsData/index.ts
+++ src/data/TestimonialsData/index.ts
@@ -1,36 +1,40 @@
 const TestimonialsData = [
-	{
-		id: 1,
-		imgProfile:
-			"https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
-		imageAlt: "Profile Img",
-		name: "Jennifer Tatiana",
-		testi: "Very easy to join the volunteer program. I am very happy to be a part of this!",
-	},
-	{
-		id: 2,
-		imgProfile:
-			"https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
-		imageAlt: "Profile Img",
-		name: "Johnny Gregorio",
-		testi: "Finally i can help people who affected by natural disaster on the spot.",
-	},
-	{
-		id: 3,
-		imgProfile:
-			"https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
-		imageAlt: "Profile Img",
-		name: "Jacob Smith",
-		testi: "Hello-Day make easier to contribute to society by helping each other via volunteer program.",
-	},
-	{
-		id: 4,
-		imgProfile:
-			"https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png",
-		imageAlt: "Profile Img",
-		name: "Angelina Jolie",
-		testi: "I'm glad i can reach more people who need help by take a part in Volunteer!",
-	},
+  {
+    id: 1,
+    imgProfile:
+      "https://images.unsplash.com/photo-1591569033002-d49b7f05338f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    imageAlt: "Profile Img",
+    name: "Jennifer Tatiana",
+    testi:
+      "Very easy to join the volunteer program. I am very happy to be a part of this!",
+  },
+  {
+    id: 2,
+    imgProfile:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    imageAlt: "Profile Img",
+    name: "Johnny Gregorio",
+    testi:
+      "Finally i can help people who affected by natural disaster on the spot.",
+  },
+  {
+    id: 3,
+    imgProfile:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    imageAlt: "Profile Img",
+    name: "Jacob Smith",
+    testi:
+      "Hello-Day make easier to contribute to society by helping each other via volunteer program.",
+  },
+  {
+    id: 4,
+    imgProfile:
+      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    imageAlt: "Profile Img",
+    name: "Howard Wolowitz",
+    testi:
+      "I'm glad i can reach more people who need help by take a part in Volunteer!",
+  },
 ];
 
 export default TestimonialsData;
